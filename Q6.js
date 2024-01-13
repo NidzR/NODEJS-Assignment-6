2e330e7115cdf0a7dceca0e5a8d3585c8f591529
Nida Rizwan <143074620+NidzR@users.noreply.github.com>
2024-01-14 02:42:17 +0500

    NODEJS Assignment Q6
    6 . Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.

---
+++ Q6.js
@@ -0,0 +1,6 @@
+"use strict";
+// 5.Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+let fname = "Rizwan\tAkhtar";
+let fname1 = "Rizwan\nAkhtar";
+console.log(fname);
+console.log(fname1);
---
+++ Q6.ts
@@ -0,0 +1,7 @@
+// 5.Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+
+let fname: string = "Rizwan\tAkhtar";
+let fname1: string = "Rizwan\nAkhtar";
+console.log(fname)
+console.log(fname1)
+
